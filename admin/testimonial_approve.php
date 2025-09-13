fe5684d2f9064fdec7b9f1d3e81cdaec8acb5022
TamimBoss <mail.tamim662@gmail.com>
2025-09-13 23:25:42 +1000

    Delete admin/testimonial_approve.php

--- admin/testimonial_approve.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-require_role('admin');
-require_once __DIR__ . '/../models/testimonials.php';
-if (!csrf_check($_POST['csrf'] ?? '')) { http_response_code(400); echo "CSRF"; exit; }
-$id = (int)($_POST['id'] ?? 0);
-$approved = (int)($_POST['approved'] ?? 0);
-if ($id) testimonial_set_approved($id,$approved);
-header("Location: /admin/index.php"); exit;
