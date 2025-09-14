7665c0c125280a8484b8ff58812251e6aaeea437
Smarika-ux <20032407@students.koi.edu.au>
2025-09-14 22:06:54 +1000

    Create resources.php

---
+++ models/resources.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../includes/config.php';
+
+function resources_all() {
+  global $pdo;
+  return $pdo->query("SELECT * FROM resources ORDER BY created_at DESC")->fetchAll();
+}
+function resources_create($type,$title,$description,$media_url) {
+  global $pdo;
+  $stmt = $pdo->prepare("INSERT INTO resources (type,title,description,media_url) VALUES (?,?,?,?)");
+  $stmt->execute([$type,$title,$description,$media_url]);
+  return $pdo->lastInsertId();
+}
+function resources_delete($id) {
+  global $pdo;
+  $stmt = $pdo->prepare("DELETE FROM resources WHERE id=?");
+  return $stmt->execute([$id]);
+}
